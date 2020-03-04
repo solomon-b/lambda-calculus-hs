0d0d60513e07fd1522e2707fa8ce60bc31f628a8
Solomon Bothwell <sbothwell@cj.com>
2020-03-04 09:36:21 -0800

    Implements System T

--- Main.hs
+++ Main.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Main where
 
+import Debug.Trace
+
 import Data.List
 import Control.Monad.Reader
 import Control.Monad.Except
@@ -13,14 +15,18 @@ data Term = Var String
           | App Term Term
           | Z
           | S Term
+          | T
+          | F
+          | If Term Term Term
+          | Rec Term Term
           | Case Term String Term Term
   deriving (Show, Eq)
 
-data Type = Type :-> Type | Nat
+data Type = Type :-> Type | Nat | Bool'
   deriving (Show, Eq)
 
 type Context = [(String, Type)]
-data TypeErr = TypeError deriving (Show, Eq)
+data TypeErr = TypeError | Debug Type deriving (Show, Eq)
 
 
 --------------------
@@ -47,7 +53,7 @@ typecheck = \case
     case ty1 of
       tyA :-> tyB -> do
         ty2 <- typecheck t2
-        if tyB == ty2 then pure ty1 else throwError TypeError
+        if tyA == ty2 then pure ty1 else throwError (Debug ty2)
       _ -> throwError TypeError
   Z -> pure Nat
   S n -> do
@@ -60,6 +66,21 @@ typecheck = \case
     if ty0 == Nat && ty1 == ty2
       then pure ty1
       else throwError TypeError
+  T -> pure Bool'
+  F -> pure Bool'
+  If t0 t1 t2 -> do
+    ty0 <- typecheck t0
+    ty1 <- typecheck t1
+    ty2 <- typecheck t2
+    if ty0 == Bool' && ty1 == ty2
+      then pure ty1
+      else throwError TypeError
+  Rec base step -> do
+    ty0 <- typecheck base
+    ty1 <- typecheck step
+    case ty1 of
+      Nat :-> (ty :-> ty') | ty == ty' -> pure (Nat :-> ty)
+      ty -> throwError $ Debug ty1
 
 
 ------------------
@@ -82,24 +103,34 @@ subst x v1 (App t1 t2) = App (subst x v1 t1) (subst x v1 t2)
 subst x v1 (Case t0 bndr t1 t2) = Case (subst x v1 t0) bndr (subst x v1 t1) (subst x v1 t2)
 subst x v1 Z = Z
 subst x v1 (S t) = S (subst x v1 t)
+subst x v1 T = T
+subst x v1 F = F
+subst x v1 (If t0 t1 t2) = If (subst x v1 t0) (subst x v1 t1) (subst x v1 t2)
+subst x v1 (Rec base step) = Rec (subst x v1 base) (subst x v1 step)
 
 freevars :: Term -> [String]
 freevars (Var x) = [x]
 freevars (Abs x _ t) = freevars t \\ [x]
 freevars (App t1 t2) = freevars t1 ++ freevars t2
-freevars Z = []
 freevars (S t) = freevars t
 freevars (Case t0 _ t1 t2) = freevars t0 ++ freevars t1 ++ freevars t2
+freevars (If t0 t1 t2) = freevars t0 ++ freevars t1 ++ freevars t2
+freevars (Rec t0 t1) = freevars t0 ++ freevars t1
+freevars _ = []
 
 singleEval :: Term -> Maybe Term
 singleEval = \case
   (App (Abs x ty t12) v2) | isVal v2 -> Just $ subst x v2 t12
   (App v1@Abs{} t2) -> App v1 <$> singleEval t2
+  (App (Rec base step) Z) -> pure base
+  (App (Rec base step) (S n)) -> pure $ App (App step n) (App (Rec base step) n)
   (App t1 t2) -> flip App t2 <$> singleEval t1
   (S t) | not (isVal t) -> S <$> singleEval t
   (Case t0 bndr t1 t2) | not (isVal t0) -> singleEval t0 >>= \t0' -> pure $ Case t0' bndr t1 t2
   (Case v1 bndr t1 t2) | v1 == Z -> pure t1
   (Case (S v1) bndr t1 t2) -> Just $ subst bndr v1 t2
+  (If T t2 t3) -> pure t2
+  (If F t2 t3) -> pure t3
   _ -> Nothing
 
 multiStepEval :: Term -> Term
@@ -116,9 +147,15 @@ isZero = Abs "n" Nat (Case (Var "n") "m" Z (S Z))
 iden :: Term
 iden = Abs "n" Nat (Var "n")
 
+add1 :: Term
+add1 = Abs "step" Nat (Abs "n" Nat (S (Var "n")))
+
+add :: Term
+add = Abs "base" Nat (Rec (Var "base") add1)
+
 main :: IO ()
 main =
-  let term = App isZero (S Z)
+  let term = App (App add (S $ S Z)) (S $ S Z)
   in case runTypecheckM $ typecheck term of
     Left e -> print e
-    Right _ -> print $ multiStepEval term
+    Right ty -> print ty >> print (multiStepEval term)
