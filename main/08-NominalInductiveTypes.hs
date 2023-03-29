3bf942926926417767cec2ffebb27da5174cb239
solomon <ssbothwell@gmail.com>
2023-03-28 21:43:42 -0700

    Improved Haddocks in 08-InductiveTypes

--- main/08-NominalInductiveTypes.hs
+++ main/08-NominalInductiveTypes.hs
@@ -52,6 +52,8 @@ alignWithM f as = traverse f . align as
 --------------------------------------------------------------------------------
 -- Types
 
+-- | 'Term' represents the concrete syntax of our langage generated
+-- from text by a parser.
 data Term
   = Var Name
   | Lam Name Term
@@ -88,12 +90,17 @@ data Type
 
 data ArgSpec
   = Term Type
-  | Rec -- [Type]
+  -- NOTE If we had type operators then this would be
+  --  | Rec -- [Type]
+  | Rec
   deriving stock (Show, Eq, Ord)
 
 data ConstrSpec
   = Constr Name [ArgSpec]
   deriving stock (Show, Eq, Ord)
+
+getCnstrName :: ConstrSpec -> Name
+getCnstrName (Constr nm _) = nm
 
 data DataSpec
   = -- If we had type variables then this would be:
@@ -105,6 +112,8 @@ data DataSpec
     Data Name [ConstrSpec]
   deriving stock (Show, Eq, Ord)
 
+-- | 'Syntax' is the internal abstract syntax of our language. We
+-- elaborate 'Term' values into 'Syntax' during typechecking.
 data Syntax
   = SVar Ix
   | SLam Name Syntax
@@ -126,6 +135,7 @@ data Syntax
   | SHole Type
   deriving stock (Show, Eq, Ord)
 
+-- | 'Value' is the evaluated form of expressions in our language.
 data Value
   = VNeutral Type Neutral
   | VLam Name Closure
@@ -142,6 +152,10 @@ data Value
 
 -- | Debruijn Indices
 --
+-- 'Ix' is used to reference lambda bound terms with respect to
+-- α-conversion. The index 'n' represents the value bound by the 'n'
+-- lambda counting outward from the site of the index.
+--
 -- λ.λ.λ.2
 -- ^-----^
 newtype Ix
@@ -150,8 +164,15 @@ newtype Ix
 
 -- | Debruijn Levels
 --
+-- Similar to Debruijn Indices but counting inward from the outermost
+-- lambda.
+--
 -- λ.λ.λ.0
 -- ^-----^
+--
+-- Levels eliminate the need to reindex free variables when weakening
+-- the context. This is useful in our 'Value' representation of
+-- lambdas where we have a 'Closure' holding a stack of free variables.
 newtype Lvl
   = Lvl Int
   deriving newtype (Show, Eq, Ord)
